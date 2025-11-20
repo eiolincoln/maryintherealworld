4a2c928edf5f36adf5771a726bf794c7d3a66c97
eiolincoln <eiolincolnstuff@gmail.com>
2025-11-20 17:20:55 +0800

    Update post.js

--- post.js
+++ post.js
@@ -1,6 +1,6 @@
 // All posts: newest first
 const posts = [
-    { title: "hello", date: "2025-11-20 5:16", text: "first post", image: "images/image10.jpg" },
+    { title: "Post 10", date: "2025-11-20 12:00", text: "Text for Post 10", image: "images/image10.jpg" },
     { title: "Post 9", date: "2025-11-19 14:00", text: "Text for Post 9", image: "images/image9.jpg" },
     { title: "Post 8", date: "2025-11-18 09:30", text: "Text for Post 8", image: "images/image8.jpg" },
     { title: "Post 7", date: "2025-11-17 20:00", text: "Text for Post 7", image: "images/image7.jpg" },
@@ -10,10 +10,6 @@ const posts = [
     { title: "Post 3", date: "2025-11-13 15:00", text: "Text for Post 3", image: "images/image3.jpg" },
     { title: "Post 2", date: "2025-11-12 08:00", text: "Text for Post 2", image: "images/image2.jpg" },
     { title: "Post 1", date: "2025-11-11 11:00", text: "Text for Post 1", image: "images/image1.jpg" },
-    { title: "Post 2", date: "2025-11-12 08:00", text: "Text for Post 2", image: "images/image2.jpg" },
-    { title: "Post 2", date: "2025-11-12 08:00", text: "Text for Post 2", image: "images/image2.jpg" },
-    { title: "Post 2", date: "2025-11-12 08:00", text: "Text for Post 2", image: "images/image2.jpg" },
-    { title: "Post 2", date: "2025-11-12 08:00", text: "Text for Post 2", image: "images/image2.jpg" },
 ];
 
 const postsPerPage = 5;
