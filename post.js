3fa5693a8d2d55e24344744011ffa655820ea86f
eiolincoln <eiolincolnstuff@gmail.com>
2025-11-21 15:19:49 +0800

    Update post.js

--- post.js
+++ post.js
@@ -4,12 +4,13 @@ const posts = [
         date:"",
         content:[
             { type: "video", value: "videos/sparkleinjamen.mp4", width: "50%" },
+            { type: "text", value: "11/21/2025 3:19pm"}
             { type: "text", value: "<br><br><br><br><br>anonymous", size: "1em" },
         ]
     },
     {
         title: "hoooly poop Ninajirachi just won ARIAs Best Solo Artist",
-        date: "20/11/2025 11:14",
+        date: "20/11/2025 11:14pm",
         content: [
             { type: "text", value: "this is super f (my computer, cuz no 1 in the World knows me Better) late but", size: "1em" },
             { type: "image", value:"images/AriaAwards2025-LiveShow-VasiliPapathanasopoulos-Ninajirachi-3-6-scaled.jpg", width: "50%" },
@@ -20,7 +21,7 @@ const posts = [
     },
     {
         title: "",
-        date: "20/11/2025 9:14",
+        date: "20/11/2025 9:14pm",
         content: [
             { type: "text", value: "eating time", size: "5em" },
             { type: "text", value: "update: this was about dinner", size: "1em" }
@@ -28,14 +29,14 @@ const posts = [
     },
     {
         title: "inventions and ideas",
-        date: "19/11/2025 7:24",
+        date: "19/11/2025 7:24pm",
         content: [
             { type: "text", value: "New bread i think it would be a good idea like most people eat bread in the mornings and i people might not like it after 5000000000000 Times of eating the same bread so imagine if there were new Bread", size: "1.1em" }
         ]
     },
     {
         title: "first post evarrrrrrrrrrrr",
-        date: "20/11/2025 5:39",
+        date: "20/11/2025 5:39pm",
         content: [
             { type: "text", value: "Been listening to I Love My Computer by Ninajirachi.<br>Favourite: Delete", size: "1em" },
             { type: "image", value: "images/ILoveMyComputer.jpg", width: "50%" },
