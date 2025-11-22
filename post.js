d211b71ea9a98f1517927b24974e5d86f0a09e9d
eiolincoln <eiolincolnstuff@gmail.com>
2025-11-23 01:07:34 +0800

    Update post.js

--- post.js
+++ post.js
@@ -3,6 +3,7 @@ const posts = [
         title: "<u>This House Has People in It</u>",
         date: "11/22/2025 12:18am",
         content: [
+            // Use the raw GitHub URLs for images
             { 
                 type: "image", 
                 value: "https://raw.githubusercontent.com/eiolincoln/maryintherealworld/main/images/niche1.png", 
@@ -30,8 +31,141 @@ const posts = [
             { type: "text", value: "not mine, looks like my grandfathers, but his is green", size: "1em" },
         ]
     },
-    // ... other posts remain unchanged
+    {
+        title: "21 Photos",
+        date: "11/21/2025 8:21pm",
+        content: [
+            { type: "image", value:"images/Screenshot1.png", width: "25%" },
+        ]
+    },
+    {
+        title: "20 Photos",
+        date: "11/21/2025 7:16pm",
+        content: [
+            { type: "text", value: "<i>77</i>", size: "1em" }
+        ]
+    },
+    {
+        title: "video test on website",
+        date:"11/21/2025 3:19pm",
+        content:[
+            { type: "video", value: "videos/sparkleinjamen.mp4", width: "30%" },
+            { type: "text", value: "11/21/2025 3:19pm anonymous", size: "1em" },
+        ]
+    },
+    {
+        title: "hoooly poop Ninajirachi just won ARIAs Best Solo Artist",
+        date: "20/11/2025 11:14pm",
+        content: [
+            { type: "text", value: "this is super f (my computer, cuz no 1 in the World knows me Better) late but", size: "1em" },
+            { type: "image", value:"images/AriaAwards2025-LiveShow-VasiliPapathanasopoulos-Ninajirachi-3-6-scaled.jpg", width: "50%" },
+            { type: "text", value: "she sniped the mother bullet into john f cuntedys head", size: "1em" },
+            { type: "image", value:"images/DiscordNinajirachiQueen.png", width: "25%" },
+            { type: "text", value: "gosh i wish listened to her music way earlier i had added f☆ck my computer in my playlist on 11th June 2025 i should've listened earlier maybe<br>could've got her on my Spotify Wrapped but it's too late<br><br>4562 4562 4562 4562 ", size: "1em" },
+        ]
+    },
+    {
+        title: "",
+        date: "20/11/2025 9:14pm",
+        content: [
+            { type: "text", value: "eating time", size: "5em" },
+            { type: "text", value: "update: this was about dinner", size: "1em" }
+        ]
+    },
+    {
+        title: "inventions and ideas",
+        date: "19/11/2025 7:24pm",
+        content: [
+            { type: "text", value: "New bread i think it would be a good idea like most people eat bread in the mornings and i people might not like it after 5000000000000 Times of eating the same bread so imagine if there were new Bread", size: "1.1em" }
+        ]
+    },
+    {
+        title: "first post evarrrrrrrrrrrr",
+        date: "20/11/2025 5:39pm",
+        content: [
+            { type: "text", value: "Been listening to I Love My Computer by Ninajirachi.<br>Favourite: Delete", size: "1em" },
+            { type: "image", value: "images/ILoveMyComputer.jpg", width: "50%" },
+            { type: "text", value: "i like this album i like how all the songs converge together like i think every song works as a interlude to the next CSIRAC to delete<br>is great cat paws up to All I Am is epic i havent gone into the second half of the album that much (aka after All I Am) except for maybe<br>like It's You nice song great album hypoppy", size: "1em" },
+            { type: "audio", value: "audio/Delete - Ninajirachi.mp3" }
+        ]
+    }
 ];
+
+    {
+        title: "",
+        date: "11/21/2025 11:39pm",
+        content: [
+            { type: "text", value: "this album sucks. i’m preparing for war.", size: "1em" }
+        ]
+    },
+    {
+        title: "<u><b>got that water in my еye eye еye</b></u>",
+        date: "11/21/2025 10:43pm",
+        content: [
+            { type: "image", value:"images/Beauty.png", width: "25%" },
+            { type: "text", value: "not mine, looks like my grandfathers, but his is green", size: "1em" },
+        ]
+    },
+    {
+        title: "21 Photos",
+        date: "11/21/2025 8:21pm",
+        content: [
+            { type: "image", value:"images/Screenshot1.png", width: "25%" },
+        ]
+    },
+    {
+        title: "20 Photos",
+        date: "11/21/2025 7:16pm",
+        content: [
+            { type: "text", value: "<i>77</i>", size: "1em" }
+        ]
+    },
+    {
+        title: "video test on website",
+        date:"11/21/2025 3:19pm",
+        content:[
+            { type: "video", value: "videos/sparkleinjamen.mp4", width: "30%" },
+            { type: "text", value: "11/21/2025 3:19pm anonymous", size: "1em" },
+        ]
+    },
+    {
+        title: "hoooly poop Ninajirachi just won ARIAs Best Solo Artist",
+        date: "20/11/2025 11:14pm",
+        content: [
+            { type: "text", value: "this is super f (my computer, cuz no 1 in the World knows me Better) late but", size: "1em" },
+            { type: "image", value:"images/AriaAwards2025-LiveShow-VasiliPapathanasopoulos-Ninajirachi-3-6-scaled.jpg", width: "50%" },
+            { type: "text", value: "she sniped the mother bullet into john f cuntedys head", size: "1em" },
+            { type: "image", value:"images/DiscordNinajirachiQueen.png", width: "25%" },
+            { type: "text", value: "gosh i wish listened to her music way earlier i had added f☆ck my computer in my playlist on 11th June 2025 i should've listened earlier maybe<br>could've got her on my Spotify Wrapped but it's too late<br><br>4562 4562 4562 4562 ", size: "1em" },
+        ]
+    },
+    {
+        title: "",
+        date: "20/11/2025 9:14pm",
+        content: [
+            { type: "text", value: "eating time", size: "5em" },
+            { type: "text", value: "update: this was about dinner", size: "1em" }
+        ]
+    },
+    {
+        title: "inventions and ideas",
+        date: "19/11/2025 7:24pm",
+        content: [
+            { type: "text", value: "New bread i think it would be a good idea like most people eat bread in the mornings and i people might not like it after 5000000000000 Times of eating the same bread so imagine if there were new Bread", size: "1.1em" }
+        ]
+    },
+    {
+        title: "first post evarrrrrrrrrrrr",
+        date: "20/11/2025 5:39pm",
+        content: [
+            { type: "text", value: "Been listening to I Love My Computer by Ninajirachi.<br>Favourite: Delete", size: "1em" },
+            { type: "image", value: "images/ILoveMyComputer.jpg", width: "50%" },
+            { type: "text", value: "i like this album i like how all the songs converge together like i think every song works as a interlude to the next CSIRAC to delete<br>is great cat paws up to All I Am is epic i havent gone into the second half of the album that much (aka after All I Am) except for maybe<br>like It's You nice song great album hypoppy", size: "1em" },
+            { type: "audio", value: "audio/Delete - Ninajirachi.mp3" }
+        ]
+    }
+];
+
 
 // --------------------------
 // PAGINATION CONFIG
