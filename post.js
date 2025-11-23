ba2ae51d945d61b31b0db2d8f13823f10e140ee3
eiolincoln <eiolincolnstuff@gmail.com>
2025-11-23 10:51:50 +0800

    Update post.js

--- post.js
+++ post.js
@@ -281,6 +281,8 @@ function initStickyEngine() {
                     entry.wrapper.remove();
                     stickyMap.delete(stickId);
                     el.style.visibility = "visible";
+                    el.style.width = "";
+                    el.style.height = "";
                 }
             }
 
