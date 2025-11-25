4d1e3a1159c6f73c794b0ac9d066aa2b6ca7f8f7
eiolincoln <eiolincolnstuff@gmail.com>
2025-11-25 19:15:02 +0800

    Update post.js

--- post.js
+++ post.js
@@ -3,7 +3,11 @@ const posts = [
         title: "",
         date: "11/25/2025 6:58pm",
         content: [
-            { type: "video", value: "videos/tayvioncole.mp4", width: "20%" },
+            {
+                type: "video",
+                value: "videos/tayvioncole.mp4",
+                width: "20%"
+            },
         ]
     },
     {
@@ -12,17 +16,194 @@ const posts = [
         content: [
             {
                 type: "text",
-                value: "1st dream i had a dream where i stabbed someone in my school ...",
+                value: "1st dream i had a dream where i stabbed someone in my school i vividly remember where but don't remember who i stabbed it was in my school it was like a hang out area for those in my school from the entrance go to the canteen, then forward to the lifts, go right and then left, go to the red block and go up by one floor the dream was i overheard between two people that in retrospect don't exist anywhere in my school nor country i for some reason overheard they were going to fight in that location after school and for some reason i went and waited there once it was time i remember sitting down and looking at the staircase and suddenly some short guy came up and shouted out HEY, WHOS FIGHTING ME? while looking left and right rapidly and on at the right side of my eye some tall guy came over like stomping and for some reason i stood up and had a knife and stabbed him around the stomach and then i suddenly felt huge pain in my stomach it was sudden and didn't carry long after waking up and i think, really think because i dont know exactly whether he actually had a knife or it was that tall guy or someone else but this whole discourse happened at the mouth of the staircase opening, then after stabbing him i felt this wave of sympathy for him and sadness for me because suddenly all of the blue his life and my life could end here, everything that we have done worked for had all came to an end because of this so i suddenly said im sorry im sorry lets go to the hospital and walked downwards the staircase while carrying each other, i think that's where the dream ended.",
                 size: "1em"
             },
             {
                 type: "text",
-                value: "2nd dream i had a dream where i was one of the founders of a drug cartel ...",
+                value: "2nd dream i had a dream where i was the one of the founders of a drug cartel i think i got this dream because i watched Alternate Breaking Bad Ending Malcolm in the Middle Edition i dont know what happened in the start to middle of the thing but I remember suddenly having this wave of fear because I would be outed one day and my life would come to a stop whether by a death or a cage till death so i tried to bargin with someone i dont remember who becuase i was the founder and there was another co founder but i bargined with them to say i did not do that much damage or provided for the cartel i just profited off it and the dream ended there",
                 size: "1em"
             }
         ]
     },
-    // ... rest of your posts
+    {
+        title: "",
+        date: "11/21/2025 9:52pm",
+        content: [
+            {
+                type: "image",
+                value: "https://raw.githubusercontent.com/eiolincoln/maryintherealworld/main/images/eerilyrealisticfunnymoments.png",
+                width: "40%"
+            }
+        ]
+    },
+    {
+        title: "THIS IS A SIGN",
+        date: "11/25/2025 9:11pm",
+        content: [
+            {
+                type: "image",
+                value: "https://raw.githubusercontent.com/eiolincoln/maryintherealworld/main/images/EllaPapo666.png",
+                width: "20%"
+            },
+            {
+                type: "image",
+                value: "https://raw.githubusercontent.com/eiolincoln/maryintherealworld/main/images/Angel666NumberNoBackground.png",
+                width: "40%"
+            },
+            { type: "text", value: "A SIGN", size: "1em" }
+        ]
+    },
+    {
+        title: "good channel",
+        date: "11/24/2025 1:57am",
+        content: [
+            {
+                type: "image",
+                value: "https://raw.githubusercontent.com/eiolincoln/maryintherealworld/main/images/Melitta.png",
+                width: "40%"
+            },
+            { type: "text", value: "titles may feel pretentious but the videos are good", size: "1em" }
+        ]
+    },
+    {
+        title: "ideal artist for the happiness of society",
+        date: "11/24/2025 1:57am",
+        content: [{ type: "text", value: "", size: "1em" }]
+    },
+    {
+        title: "",
+        date: "11/23/2025 11:07pm",
+        content: [{ type: "text", value: "edited a video today", size: "1em" }]
+    },
+    {
+        title: "<u>This House Has People in It</u>",
+        date: "11/22/2025 12:18am",
+        content: [
+            {
+                type: "image",
+                value: "https://raw.githubusercontent.com/eiolincoln/maryintherealworld/main/images/niche1.png",
+                width: "25%"
+            },
+            {
+                type: "image",
+                value: "https://raw.githubusercontent.com/eiolincoln/maryintherealworld/main/images/niche2.png",
+                width: "30%"
+            }
+        ]
+    },
+    {
+        title: "",
+        date: "11/21/2025 11:39pm",
+        content: [
+            { type: "text", value: "this album sucks. i’m preparing for war.", size: "1em" }
+        ]
+    },
+    {
+        title: "<u><b>got that water in my еye eye еye</b></u>",
+        date: "11/21/2025 10:43pm",
+        content: [
+            { type: "image", value: "images/Beauty.png", width: "10%" },
+            { type: "text", value: "not mine, looks like my grandfathers, but his is green", size: "1em" }
+        ]
+    },
+    {
+        title: "21 Photos",
+        date: "11/21/2025 8:21pm",
+        content: [{ type: "image", value: "images/Screenshot1.png", width: "25%" }]
+    },
+    {
+        title: "20 Photos",
+        date: "11/21/2025 7:16pm",
+        content: [{ type: "text", value: "<i>77</i>", size: "1em" }]
+    },
+    {
+        title: "video test on website",
+        date: "11/21/2025 3:19pm",
+        content: [
+            {
+                type: "video",
+                value: "videos/sparkleinjamen.mp4",
+                width: "30%"
+            },
+            { type: "text", value: "11/21/2025 3:19pm anonymous", size: "1em" }
+        ]
+    },
+    {
+        title: "hoooly poop Ninajirachi just won ARIAs Best Solo Artist",
+        date: "20/11/2025 11:14pm",
+        content: [
+            {
+                type: "text",
+                value: "this is super f (my computer, cuz no 1 in the World knows me Better) late but",
+                size: "1em"
+            },
+            {
+                type: "image",
+                value: "images/AriaAwards2025-LiveShow-VasiliPapathanasopoulos-Ninajirachi-3-6-scaled.jpg",
+                width: "50%"
+            },
+            {
+                type: "text",
+                value: "she sniped the mother bullet into john f cuntedys head",
+                size: "1em"
+            },
+            {
+                type: "image",
+                value: "images/DiscordNinajirachiQueen.png",
+                width: "25%"
+            },
+            {
+                type: "text",
+                value: "gosh i wish listened to her music way earlier i had added f☆ck my computer in my playlist on 11th June 2025 i should've listened earlier maybe<br>could've got her on my Spotify Wrapped but it's too late<br><br>4562 4562 4562 4562 ",
+                size: "1em"
+            }
+        ]
+    },
+    {
+        title: "",
+        date: "20/11/2025 9:14pm",
+        content: [
+            { type: "text", value: "eating time", size: "5em" },
+            { type: "text", value: "update: this was about dinner", size: "1em" }
+        ]
+    },
+    {
+        title: "inventions and ideas",
+        date: "20/11/2025 7:24pm",
+        content: [
+            {
+                type: "text",
+                value: "New bread i think it would be a good idea like most people eat bread in the mornings and i people might not like it after 5000000000000 Times of eating the same bread so imagine if there were new Bread",
+                size: "1.1em"
+            }
+        ]
+    },
+    {
+        title: "first post evarrrrrrrrrrrr",
+        date: "20/11/2025 5:39pm",
+        content: [
+            {
+                type: "text",
+                value: "Been listening to I Love My Computer by Ninajirachi.<br>Favourite: Delete",
+                size: "1em"
+            },
+            {
+                type: "image",
+                value: "images/ILoveMyComputer.jpg",
+                width: "50%"
+            },
+            {
+                type: "text",
+                value: "i like this album i like how all the songs converge together like i think every song works as a interlude to the next CSIRAC to delete<br>is great cat paws up to All I Am is epic i havent gone into the second half of the album that much (aka after All I Am) except for maybe<br>like It's You nice song great album hypoppy",
+                size: "1em"
+            },
+            {
+                type: "audio",
+                value: "audio/Delete - Ninajirachi.mp3"
+            }
+        ]
+    }
 ];
 
 // --------------------------
